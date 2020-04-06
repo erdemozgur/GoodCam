14d631d0fd0ff28ac31d03ebd6fc4af81e3695bd
erdem ozgur <erdmzgr@gmail.com>
2020-04-06 11:39:41 +0300

    Requesting User's Permission for accessing photo library on phone
    Requesting User's Permission for accessing photo library on phone

--- GoodCam/Controllers/PhotoListCollectionViewController.swift
+++ GoodCam/Controllers/PhotoListCollectionViewController.swift
@@ -8,13 +8,35 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotoListCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    private func requestPermission(completion: @escaping(PHAuthorizationStatus) -> ()) {
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            DispatchQueue.main.async {
+                completion(status)
+            }
+        }
         
     }
     
     
+    private func populatePhotos() {
+        
+        requestPermission { status in
+            if status == .authorized {
+                print("authorized")
+            }
+        }
+        
+        
+    }
     
 }
